5a7fa7720caf2523cb4ebc6d56a632dad9a20833
Kevyn <kevyn.buchet@gmail.com>
2019-01-24 17:09:34 +0100

    Ajout des fonctions login, getContacts, addContact, updateContact, deleteContact

---
+++ Familink/API/APIClient.swift
@@ -0,0 +1,243 @@
+//
+//  APIClient.swift
+//  Familink
+//
+//  Created by formation9 on 24/01/2019.
+//  Copyright © 2019 CubaLibre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class APIClient {
+    static let instance = APIClient()
+    private let BASE_URL = "https://familink-api.cleverapps.io/"
+    private let LOGIN_URI = "public/login/"
+    private let ADD_USER_URI = "/public/sign-in/"
+    private let CONTACT_URI = "secured/users/contacts/"
+    private let TOKEN = "token"
+    private let PHONE = "phone"
+    private let PASSWORD = "password"
+    private let FIRSTNAME = "firstName"
+    private let LASTNAME = "lastName"
+    private let EMAIL = "email"
+    private let PROFILE = "profile"
+    private let GRAVATAR = "gravatar"
+    private let IS_FAMILINK_USER = "isFamilinkUser"
+    private let IS_EMERGENCY_USER = "isEmergencyUser"
+    private let CONTACT_ID = "_id"
+    
+    private init() {
+    }
+    
+    func getToken(login : String, password : String, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        let json : [String: Any] = [self.PHONE : login, self.PASSWORD : password]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(LOGIN_URI)")! )
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 200){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    if let dataResponse = data {
+                        if let jsonResponse = try? JSONSerialization.jsonObject(with: dataResponse, options: [])  as! [String: String] {
+                            let token = jsonResponse["token"] ?? ""
+                            onSuccess(token)
+                        }
+                    } else {
+                        onError("Erreur")
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func getContactList(token : String, onSuccess: @escaping ([Contact]) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(CONTACT_URI)")! )
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer: \(token)", forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 200){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    if let dataResponse = data {
+                        if let jsonResponse = try? JSONSerialization.jsonObject(with: dataResponse, options: [])  as! [Any] {
+                            var contactsToReturn = [Contact]()
+                            
+                            for object in jsonResponse {
+                                let objectDictionary = object as! [String:Any]
+                                
+                                let c = Contact(context: self.getContext()!)
+                                c.contactId = objectDictionary[self.CONTACT_ID] as? String
+                                c.firstName = objectDictionary[self.FIRSTNAME] as? String
+                                c.lastName = objectDictionary[self.LASTNAME] as? String ?? ""
+                                c.phone = objectDictionary[self.PHONE] as? String
+                                c.email = objectDictionary[self.EMAIL] as? String
+                                c.gravatar = objectDictionary[self.GRAVATAR] as? String ?? ""
+                                c.profile = objectDictionary[self.PROFILE] as? String ?? ""
+                                c.isFamilinkUser = objectDictionary[self.IS_FAMILINK_USER] as! Bool ?? false
+                                c.isEmergencyUser = objectDictionary[self.IS_EMERGENCY_USER] as! Bool ?? false
+                                
+                                contactsToReturn.append(c)
+                            }
+                            onSuccess(contactsToReturn)
+                        }
+                    } else {
+                        onError("Erreur")
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func addContact(token : String, contact : Contact, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        let jsonData = createJsonDataFromContact(contactToParseInJson : contact)
+        
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(CONTACT_URI)")! )
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer: \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 200){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    onSuccess("Contact ajouté")
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func updateContact(token : String, contact : Contact, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        let jsonData = createJsonDataFromContact(contactToParseInJson : contact)
+        
+        print(contact)
+        
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(CONTACT_URI)\(contact.contactId!)")! )
+        request.httpMethod = "PUT"
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer: \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 204){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    onSuccess("Contact modifié")
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func deleteContact(token : String, contactId : String, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(CONTACT_URI)\(contactId)")! )
+        request.httpMethod = "DELETE"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer: \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 204){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    onSuccess("Contact supprimé")
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func addUser(token : String, user : User, password : String, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        let jsonData = createJsonDataFromUser(userToParseInJson: user, password: password)
+        
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(CONTACT_URI)\(ADD_USER_URI)")! )
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer: \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode != 200){
+                    onError(self.getStatusError(statusCode: httpResponse.statusCode))
+                } else {
+                    onSuccess("User ajouté")
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func createJsonDataFromContact(contactToParseInJson : Contact) -> Data? {
+        let json : [String: Any] = [self.CONTACT_ID : contactToParseInJson.contactId ?? "",
+                                    self.FIRSTNAME : contactToParseInJson.firstName ?? "",
+                                    self.LASTNAME : contactToParseInJson.lastName ?? "",
+                                    self.PHONE : contactToParseInJson.phone ?? "",
+                                    self.EMAIL : contactToParseInJson.email ?? "",
+                                    self.GRAVATAR : contactToParseInJson.gravatar ?? "",
+                                    self.IS_FAMILINK_USER : contactToParseInJson.isFamilinkUser,
+                                    self.IS_EMERGENCY_USER : contactToParseInJson.isEmergencyUser]
+        
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        return jsonData
+    }
+    
+    func createJsonDataFromUser(userToParseInJson : User, password : String) -> Data? {
+        let json : [String: Any] = [self.FIRSTNAME : userToParseInJson.firstName ?? "",
+                                    self.LASTNAME : userToParseInJson.lastName ?? "",
+                                    self.PHONE : userToParseInJson.phone ?? "",
+                                    self.EMAIL : userToParseInJson.email ?? "",
+                                    self.PASSWORD : password ?? "",
+                                    self.PROFILE : userToParseInJson.profile]
+        
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        return jsonData
+    }
+    
+    func getStatusError(statusCode : Int) -> String {
+        switch statusCode {
+        case 400:
+            return "Identifiant invalide"
+        case 401:
+            return "Session expirée"
+        default:
+            return "Serveur inaccessible"
+        }
+    }
+    
+    func getContext() -> NSManagedObjectContext? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        return appDelegate.persistentContainer.viewContext
+    }
+}
--- Familink/ViewController.swift
+++ Familink/ViewController.swift
@@ -9,12 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    private var token : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTapButton(_ sender: Any) {
+        
+        APIClient.instance.getToken(login: self.login.text ?? "", password: self.password.text ?? "", onSuccess: { (token) in
+            self.token = token
+            print("Token is ok")
+        }) { (error) in
+            print(error)
+        }
+    }
+    
+    @IBAction func onGetListeButton(_ sender: Any) {
+        APIClient.instance.getContactList(token: self.token, onSuccess: { (contacts) in
+            print(contacts)
+        }) { (error) in
+            print(error)
+        }
+    }
+    
+    @IBAction func onAddButton(_ sender: Any) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let c = Contact(context: context)
+        c.firstName = "aaaaaaa"
+        c.lastName = "jhghg"
+        c.phone = "0618948899"
+        c.email = "gfgf@hf"
+        c.gravatar = "http://aaaaa"
+        c.profile = "FAMILLE"
+        c.isEmergencyUser = true
+        c.isFamilinkUser = false
+        
+        
+        APIClient.instance.addContact(token: self.token, contact: c, onSuccess: { (success) in
+            print(success)
+        }) { (error) in
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func onDeleteButton(_ sender: Any) {
+        APIClient.instance.deleteContact(token: self.token, contactId: "5c49ca6565ecd90866845a03", onSuccess: { (success) in
+            print(success)
+        }) { (error) in
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func onUpdateButton(_ sender: Any) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let c = Contact(context: context)
+        c.contactId = "5c49c8bf65ecd90866845a01"
+        c.firstName = "Update Kevyn"
+        c.lastName = "Update"
+        c.phone = "0618728899"
+        c.email = "update@hf"
+        c.gravatar = "http://update"
+        c.profile = "FAMILLE"
+        c.isEmergencyUser = true
+        c.isFamilinkUser = false
+        
+        APIClient.instance.updateContact(token: self.token, contact: c, onSuccess: { (success) in
+            print(success)
+        }) { (error) in
+            print(error)
+        }
+        
+    }
 }
-
