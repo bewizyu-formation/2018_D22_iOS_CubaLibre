03f7c3a857e7336137a20e9d0509acb8f203d2d5
Kevyn <kevyn.buchet@gmail.com>
2019-01-28 12:35:34 +0100

    Ajout de comportement sur l'écran de login

--- Familink/ViewControllers/LoginUIViewController.swift
+++ Familink/ViewControllers/LoginUIViewController.swift
@@ -12,48 +12,61 @@ class LoginUIViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
-    @IBOutlet weak var loginConstraint: NSLayoutConstraint!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.isNavigationBarHidden = true
         
+        self.initViewUI()
         NotificationCenter.default.addObserver(self, selector: #selector(onDidUserDisconnect(_:)), name: .didUserDisconnect, object: nil)
-        // Do any additional setup after loading the view.
-        
-        logInButton.layer.cornerRadius = logInButton.frame.height/2
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
     @IBAction func onSignUpButtonTap(_ sender: Any) {
         let signUpController = SignUpViewController(nibName:nil, bundle: nil)
         self.navigationController?.pushViewController(signUpController, animated: true)
+        self.navigationController?.isNavigationBarHidden = false
     }
     
     @IBAction func onForgotPasswordButtonTap(_ sender: Any) {
         let forgotPasswordController = ForgottenPasswordViewController(nibName:nil, bundle: nil)
         self.navigationController?.pushViewController(forgotPasswordController, animated: true)
+        self.navigationController?.isNavigationBarHidden = false
     }
     
-    @IBAction func userNav(_ sender: Any) {
-    }
     @IBAction func onLoginButtonTap(_ sender: Any) {
         UIView.animate(withDuration: -1, animations: {
             self.view.layoutIfNeeded()
         }) { (_) in
             APIClient.instance.getToken(login: self.loginTextField.text ?? "", password: self.passwordTextField.text ?? "", onSuccess: { (success) in
                 DispatchQueue.main.async {
-                    UIView.animate(withDuration: -1, animations: {
+                    UIView.animate(withDuration: 2, animations: {
                         self.logInButton.setTitle("Connecté", for: .normal)
-                        self.logInButton.titleLabel?.alpha = 1
                         self.logInButton.backgroundColor = .green
-                        NotificationCenter.default.post(name: .didUserConnect, object: nil)
+                        self.loginBottomConstraint.constant = 0
+                        UIView.animate(withDuration: 2, animations: {
+                            self.loginTextField.isHidden = true
+                            self.passwordTextField.isHidden = true
+                            self.forgotPasswordButton.isHidden = true
+                            self.signUpButton.isHidden = true
+                            self.view.layoutIfNeeded()
+                            NotificationCenter.default.post(name: .didUserConnect, object: nil)
+                        })
                     })
                 }
             }) { (error) in
                 DispatchQueue.main.async {
                     UIView.animate(withDuration: -1, animations: {
-                        self.logInButton.setTitle("Erreur", for: .normal)
-                        self.logInButton.titleLabel?.alpha = 1
+                        self.logInButton.setTitle("Réessayer", for: .normal)
                         self.logInButton.backgroundColor = .red
+                        let alert = UIAlertController(title: error, message: "", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                        self.present(alert, animated: true)
                     })
                 }
             }
@@ -63,9 +76,41 @@ class LoginUIViewController: UIViewController {
     
     @objc func onDidUserDisconnect(_ notification:Notification) {
         print("disconnet")
+        self.navigationController?.isNavigationBarHidden = true
+        self.logInButton.backgroundColor = UIColor.rosyBrown
         self.logInButton.setTitle("Se connecter", for: .normal)
+        self.loginBottomConstraint.constant = 120
+        
+        self.loginTextField.isHidden = false
+        self.passwordTextField.isHidden = false
+        self.forgotPasswordButton.isHidden = false
+        self.signUpButton.isHidden = false
+        
         self.navigationController?.popToRootViewController(animated: false)
     }
+    
+    func initViewUI() {
+        self.navigationController?.isNavigationBarHidden = true
+        self.view.backgroundColor = .seaShell
+        
+        self.loginTextField.textColor = .rosyBrown
+        self.loginTextField.layer.borderColor = UIColor.rosyBrown.cgColor
+        self.loginTextField.layer.borderWidth = 1.0
+        self.loginTextField.layer.cornerRadius = self.loginTextField.frame.height/2
+        
+        self.passwordTextField.textColor = .rosyBrown
+        self.passwordTextField.layer.borderColor = UIColor.rosyBrown.cgColor
+        self.passwordTextField.layer.borderWidth = 1.0
+        self.passwordTextField.layer.cornerRadius = self.passwordTextField.frame.height/2
+        
+        self.logInButton.layer.cornerRadius = logInButton.frame.height/2
+        self.logInButton.backgroundColor = .oldRose
+        
+        self.signUpButton.setTitleColor(.oldRose, for: .normal)
+        self.forgotPasswordButton.setTitleColor(.oldRose, for: .normal)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
