81a52a63c5053d52c49530acca7da629185e0529
Kevyn <kevyn.buchet@gmail.com>
2019-01-31 17:38:55 +0100

    Ajout de la fonction forgotPassword dans l'api

--- Familink/API/APIClient.swift
+++ Familink/API/APIClient.swift
@@ -18,6 +18,7 @@ class APIClient {
     private let UPDATE_USER_URI = "secured/users"
     private let CURRENT_USER_URI = "secured/users/current"
     private let CONTACT_URI = "secured/users/contacts/"
+    private let FORGOT_PASSWORD = "public/forgot-password"
     private let TOKEN = "token"
     private let PHONE = "phone"
     private let PASSWORD = "password"
@@ -122,7 +123,14 @@ class APIClient {
                 if(httpResponse.statusCode == 200){
                     onSuccess("Contact ajouté")
                 } else {
-                    onError(self.getError(data: data))
+                    if(httpResponse.statusCode == 401){
+                        print("expirée")
+                        NotificationCenter.default.post(name: .didUserDisconnect, object: nil)
+                        onError("La session a expirée")
+                    } else {
+                        print(error)
+                        onError(self.getError(data: data))
+                    }
                 }
             }
         }
@@ -262,6 +270,29 @@ class APIClient {
                     } else {
                         onError("Erreur inconnue")
                     }
+                } else {
+                    onError(self.getError(data: data))
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
+    func forgotPassword(phone : String, onSuccess: @escaping (String) -> (), onError: @escaping (String) -> ()) -> URLSessionTask {
+        let json : [String: Any] = [self.PHONE : phone]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        var request = URLRequest(url: URL(string: "\(BASE_URL)\(FORGOT_PASSWORD)")! )
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse {
+                if(httpResponse.statusCode == 200){
+                    onSuccess("Vous recevrez votre nouveau mot de passe par mail")
                 } else {
                     onError(self.getError(data: data))
                 }
